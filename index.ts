180b03dbb1384553f4b92b7d9d4f2c79c4a88482
Nikhil-Reddy-Karukonda <karukonda.n@northeastern.edu>
2023-10-12 15:49:29 -0400

    fetch AZ, create public and private subnets, configure IG

--- index.ts
+++ index.ts
@@ -30,3 +30,88 @@ const vpc = new aws.ec2.Vpc(vpcName, {
         Name: vpcName,
     },
 });
+
+aws.getAvailabilityZones().then(azs => {
+    const azsToUse = azs.names.slice(0, maxAvailabilityZones!);
+
+    const internetGateway = new aws.ec2.InternetGateway(internetGatewayName, {
+        tags: {
+            Name: internetGatewayName,
+        },
+    });
+
+    const igAttachment = new aws.ec2.InternetGatewayAttachment(internetGatewayAttachmentName, {
+        vpcId: vpc.id,
+        internetGatewayId: internetGateway.id,
+    });
+
+
+    const totalSubnets = azsToUse.length * 2;
+    const subnetCIDRs = calculateCIDRSubnets(vpcCidr, totalSubnets, bitsForEachSubnet!);
+
+    if (subnetCIDRs instanceof Error) {
+        throw new pulumi.RunError("Failed to calculate subnet CIDRs: " + subnetCIDRs.message);
+    }
+
+    let publicSubnets: aws.ec2.Subnet[] = [];
+    let privateSubnets: aws.ec2.Subnet[] = [];
+
+    for (let i = 0; i < azsToUse.length; i++) {
+        const publicSubnet = new aws.ec2.Subnet(`${publicSubnetsPrefix}-${i}`, {
+            vpcId: vpc.id,
+            availabilityZone: azsToUse[i],
+            cidrBlock: subnetCIDRs[i],
+            tags: {
+                Name: `${publicSubnetsPrefix}-${i}`,
+            },
+        });
+
+        publicSubnets.push(publicSubnet);
+
+        const privateSubnet = new aws.ec2.Subnet(`${privateSubnetsPrefix}-${i}`, {
+            vpcId: vpc.id,
+            availabilityZone: azsToUse[i],
+            cidrBlock: subnetCIDRs[azsToUse.length + i],
+            tags: {
+                Name: `${privateSubnetsPrefix}-${i}`,
+            },
+        });
+
+        privateSubnets.push(privateSubnet);
+    }
+
+});
+
+
+function calculateCIDRSubnets(parentCIDR: string, numSubnets: number, bitsToMask: number): string[] | Error {
+    try {
+        if (bitsToMask > 32) {
+            throw new Error("Bits to mask exceeds the available bits in the parent CIDR");
+        }
+
+        function ipToInt(ip: string): number {
+            return ip.split('.').reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0) >>> 0;
+        }
+
+        function intToIp(int: number): string {
+            return [(int >>> 24) & 0xFF, (int >>> 16) & 0xFF, (int >>> 8) & 0xFF, int & 0xFF].join('.');
+        }
+
+        const subnetSize = 1 << (32 - bitsToMask);
+        const ipRange = ip.cidrSubnet(parentCIDR);
+        let baseIpInt = ipToInt(ipRange.networkAddress);
+
+        const subnets: string[] = [];
+
+        for (let i = 0; i < numSubnets; i++) {
+            const subnetCIDR = intToIp(baseIpInt) + "/" + bitsToMask;
+            subnets.push(subnetCIDR);
+            baseIpInt += subnetSize;
+        }
+
+        return subnets;
+    } catch (error) {
+        return error as Error;
+    }
+}
+
